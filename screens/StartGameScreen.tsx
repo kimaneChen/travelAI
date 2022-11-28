5d5145c0ad9417ddb16cf6b7e45d4100d03d2c14
Kimane <chenjx1024@outlook.com>
2022-11-28 14:59:37 +1000

    fix/solve the style

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -5,7 +5,7 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.input} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,10 +16,26 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     padding: 16,
     marginTop: 100,
-    backgroundColor: 'red',
-    alignItems: 'center',
+    marginHorizontal: 24,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 6,
+  },
+  input: {
+    height: 50,
+    width: 300,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
