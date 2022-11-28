41c01725009152ffb9b3ae5b246d6ef151bb857e
Kimane <chenjx1024@outlook.com>
2022-11-28 15:20:12 +1000

    feat/add button style

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,19 +1,36 @@
 import React, { ReactElement } from 'react';
-import { View, Text, Pressable, PressableProps } from 'react-native';
-
-// export interface PrimaryButtonProps extends PressableProps {
-//   test: ReactElement | ReactElement[] | string | number;
-// }
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton(props: {
   children: ReactElement | ReactElement[] | string | number;
 }) {
   const { children } = props;
+  function pressHandler() {
+    console.log('eeee');
+  }
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable onPress={pressHandler} style={styles.container}>
+      <View>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#72063c',
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    margin: 4,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
