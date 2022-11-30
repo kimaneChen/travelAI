1df7ec3f4ae844f67096d676bc93a7786f22ee54
Kimane <chenjx1024@outlook.com>
2022-11-30 15:34:20 +1000

    feat/over

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,15 +1,43 @@
 /* eslint-disable global-require */
-import { Image, View } from 'react-native';
+import { Image, View, StyleSheet } from 'react-native';
 
 import Title from '../components/ui/Title';
+import Colors from '../constants/color';
 
 function GameOverScreen() {
   return (
-    <View>
+    <View style={styles.screenContainer}>
       <Title>Game Over!</Title>
-      <Image source={require('../assets/images/success.png')} />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
     </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
